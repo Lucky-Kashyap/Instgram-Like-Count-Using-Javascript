62cd123d22aa6da41bbcfd1b3a27224d2b21e5d6
Lucky Kashyap <88204554+Lucky-Kashyap@users.noreply.github.com>
2022-08-03 09:57:58 +0530

    Instragram Double Tap To Like Count
    Double Tap To Like Count Cool Functionality Using JavaScript

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+const clickArea = document.querySelector(".inner-div");
+
+const clickCount = document.querySelector('.clickCount');
+
+let likeCount = 0;
+
+const showHeart = (e)=>{
+
+    const heart  = document.createElement("span");
+
+    heart.classList.add("fa");
+    heart.classList.add("fa-heart");
+    clickArea.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },800);
+}
+
+clickArea.addEventListener('dblclick',(e)=>{
+    clickCount.innerHTML = ++likeCount;
+
+
+    showHeart(e);
+});
